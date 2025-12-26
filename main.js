e065bf4b631f42620eaf2d84b683807220212fc8
surailetisim <turkcell.surailetisim@gmail.com>
2025-12-26 13:24:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-document.getElementById("applyForm")?.addEventListener("submit", function(e) {
-    e.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const phone = document.getElementById("phone").value;
-    const type = document.getElementById("type").value;
-    const note = document.getElementById("note").value;
-
-    const whatsappNumber = "905305162948";
-
-    const message = `Merhaba, Turkcell hat başvurusu yapmak istiyorum.%0AAd Soyad: ${name}%0ATelefon: ${phone}%0AHat Türü: ${type}%0ANot: ${note}`;
-    const whatsappURL = `https://wa.me/${whatsappNumber}?text=${message}`;
-
-    window.open(whatsappURL, "_blank");
-});
